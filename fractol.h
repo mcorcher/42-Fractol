f40a550f658df3b7f366315f378d4d8499471940
mcorcher <mateocorcherorivas@gmail.com>
2025-01-24 16:14:16 +0100

    Fixing norminette

--- fractol.h
+++ fractol.h
@@ -29,28 +29,28 @@
 # define HEIGHT	800
 // Colores
 // Colores básicos
-#define BLACK         0x000000  // Negro
-#define WHITE         0xFFFFFF  // Blanco
-#define RED           0xFF0000  // Rojo
-#define GREEN         0x00FF00  // Verde
-#define BLUE          0x0000FF  // Azul
-#define YELLOW        0xFFFF00  // Amarillo
-#define CYAN          0x00FFFF  // Cian
-#define MAGENTA       0xFF00FF  // Magenta
-#define GRAY          0x808080  // Gris
-#define ORANGE        0xFFA500  // Naranja
+# define BLACK         0x000000  // Negro
+# define WHITE         0xFFFFFF  // Blanco
+# define RED           0xFF0000  // Rojo
+# define GREEN         0x00FF00  // Verde
+# define BLUE          0x0000FF  // Azul
+# define YELLOW        0xFFFF00  // Amarillo
+# define CYAN          0x00FFFF  // Cian
+# define MAGENTA       0xFF00FF  // Magenta
+# define GRAY          0x808080  // Gris
+# define ORANGE        0xFFA500  // Naranja
 
 // Colores psicodélicos
-#define NEON_PINK      0xFF10F0  // Rosa neón
-#define ELECTRIC_LIME  0xCCFF00  // Lima eléctrica
-#define CYBER_PURPLE   0x8000FF  // Púrpura cibernético
-#define HOT_MAGENTA    0xFF00CC  // Magenta intenso
-#define VIVID_ORANGE   0xFF4500  // Naranja vívido
-#define ACID_GREEN     0x7FFF00  // Verde ácido
-#define ULTRA_BLUE     0x0011FF  // Azul ultra
-#define PSYCHEDELIC_RED 0xFF0066 // Rojo psicodélico
-#define GLOW_YELLOW    0xFFFF33  // Amarillo brillante
-#define TROPICAL_AQUA  0x33FFF3  // Agua tropical
+# define NEON_PINK      0xFF10F0  // Rosa neón
+# define ELECTRIC_LIME  0xCCFF00  // Lima eléctrica
+# define CYBER_PURPLE   0x8000FF  // Púrpura cibernético
+# define HOT_MAGENTA    0xFF00CC  // Magenta intenso
+# define VIVID_ORANGE   0xFF4500  // Naranja vívido
+# define ACID_GREEN     0x7FFF00  // Verde ácido
+# define ULTRA_BLUE     0x0011FF  // Azul ultra
+# define PSYCHEDELIC_RED 0xFF0066 // Rojo psicodélico
+# define GLOW_YELLOW    0xFFFF33  // Amarillo brillante
+# define TROPICAL_AQUA  0x33FFF3  // Agua tropical
 
 // Valores complejos, x=REAL , y=IMAGINARIO
 typedef struct s_complex
@@ -97,7 +97,7 @@ t_complex	sum_complex(t_complex z1, t_complex z2);
 t_complex	square_complex(t_complex z);
 int			key_handler(int keysym, t_fractal *fractal);
 int			close_handler(t_fractal *fractal);
-int			mouse_handler(int button,int x, int y, t_fractal *fractal);
+int			mouse_handler(int button, int x, int y, t_fractal *fractal);
 int			julia_track(int x, int y, t_fractal *fractal);
 
 #endif
